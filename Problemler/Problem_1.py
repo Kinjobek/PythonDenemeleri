23fdc4e8aca02e01c897498d172037d7aa8d3695
M. E. Berkay KOCAOĞLU <50452706+mebon@users.noreply.github.com>
2019-07-24 23:54:41 +0300

    Verilen 1.problem çözüldü.

--- Problemler/Problem_1.py
+++ Problemler/Problem_1.py
@@ -1,4 +1,13 @@
 """
 Kullanıcıdan aldığınız 3 tane sayıyı çarparak ekrana yazdırın. Ekrana yazdırma işlemini format metoduyla yapmaya çalışın.
 """
-print("developer 1")
+
+print("Lütfen çarpılmasını istediğiniz 3 sayıyı giriniz.")
+sayi1 = int(input("Sayı 1 : "))
+sayi2 = int(input("Sayı 2 : "))
+sayi3 = int(input("Sayı 3 : "))
+
+"Sayılar çarpılıyor....."
+print("Sayıların çarpımı : {}".format(sayi1*sayi2*sayi3))
+
+#Problem çözüldü.
