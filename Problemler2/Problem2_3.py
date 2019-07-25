31fdeebd0d3f28d0821a61bf3207969e244eda40
M. E. Berkay KOCAOĞLU <50452706+mebon@users.noreply.github.com>
2019-07-25 18:10:31 +0300

    Problem2_3.py için alan oluşturuldu.

---
+++ Problemler2/Problem2_3.py
@@ -0,0 +1,28 @@
+"""
+Kullanıcının girdiği vize1,vize2,final notlarına notlarına göre harf notunu hesaplayın.
+
+Vize1 toplam notun %30'una etki edecek.
+
+Vize2 toplam notun %30'una etki edecek.
+
+Final toplam notun %40'ına etki edecek.
+
+
+Toplam Not >=  90 -----> AA
+
+Toplam Not >=  85 -----> BA
+
+Toplam Not >=  80 -----> BB
+
+Toplam Not >=  75 -----> CB
+
+Toplam Not >=  70 -----> CC
+
+Toplam Not >=  65 -----> DC
+
+Toplam Not >=  60 -----> DD
+
+Toplam Not >=  55 -----> FD
+
+Toplam Not <  55 -----> FF
+"""
