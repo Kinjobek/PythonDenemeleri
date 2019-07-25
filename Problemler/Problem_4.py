e3683bf31db2901fc6a56e4af5ff0daeeaacf84b
M. E. Berkay KOCAOĞLU <50452706+mebon@users.noreply.github.com>
2019-07-25 12:58:39 +0300

    4.problem çözüldü.

--- Problemler/Problem_4.py
+++ Problemler/Problem_4.py
@@ -1,3 +1,13 @@
 """
 Kullanıcıdan ad,soyad ve numara bilgisini alarak bunları alt alta ekrana yazdırın.
 """
+isim = input("İsim : ")
+soyisim = input("Soyisim : ")
+telefonNumarası = input("Tel No : ")
+print("Print içerisinde ',' koyarak sırasıyla yazdırmak")
+print("İsim : ",isim,"\nSoyisim : ",soyisim,"\nTelefon Numarası : ",telefonNumarası)
+#veya
+print("Print içerisinde formatlama ve sep=""\\n"" ile yazdırmak ")
+print("İsim : {}".format(isim),"Soyisim : {}".format(soyisim),"Telefon Numarası : {}".format(telefonNumarası),sep="\n")
+
+#Problem Çözüldü.
