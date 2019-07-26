9f4ba627a207b581ad661e208be3347c4bd1928a
M. E. Berkay KOCAOĞLU <50452706+mebon@users.noreply.github.com>
2019-07-26 09:39:43 +0300

    Problem2_1 çözüldü.

--- Problemler2/Problem2_1.py
+++ Problemler2/Problem2_1.py
@@ -11,3 +11,21 @@ BKİ 25 ile 30 arasındaysa --------> Fazla Kilolu
 
 BKİ 30'un üstündeyse -------------> Obez
 """
+print("***************************\n"
+      "    Beden Kitle İndeksi\n"
+      "***************************")
+boy = int(input("Lütfen boy(cm) giriniz(örneğin 172):"))
+kilo = int(input("Lütfen kilo(kg) giriniz(örneğin 70):"))
+print("Hesaplanıyor.....")
+bki =float("{:.2f}".format(kilo/((boy/100)**2)))
+print("Beden kitle indeksiniz :{}".format(bki))
+if bki<18.5 :
+    print("Zayıfsın. Dengeli beslenerek ve düzgün spor yaparak kilonu arttırmalısın.")
+elif bki>18.5 and bki<25 :
+    print("Gayet normalsin. Yediklerini abartma ve düzenli yürüyüş, spor ile kilonu koru.")
+elif bki>25 and bki<30 :
+    print("Şişkosun. Yemeyi içmeyi kes ve hemen kalkıp yürümeye başla.")
+elif bki>30:
+    print("Obezsin. Acilen bir doktora görünmelisin.")
+
+#Problem çözüldü
