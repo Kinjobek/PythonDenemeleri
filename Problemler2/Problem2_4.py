6cf5ac78dac628ca5b63d9b3ad1f05dbdee3ac62
M. E. Berkay KOCAOĞLU <50452706+mebon@users.noreply.github.com>
2019-07-26 12:06:23 +0300

    Problem2_4.py çözüldü.

--- Problemler2/Problem2_4.py
+++ Problemler2/Problem2_4.py
@@ -15,3 +15,49 @@ Kullanımı şu şekildedir ;
 abs(-4)
 4
 """
+print("**********************************\n"
+      "    Geometrik Şekil Hesaplama\n"
+      "**********************************\n"
+      "1) Dörtgen için 1 yazınız.\n"
+      "2) Üçgen için 2 yazınız.\n"
+      "**********************************")
+kenar1 = None
+kenar2 = None
+kenar3 = None
+kenar4 = None
+while True:
+    secenek = input("Seçeneğinizi giriniz.")
+    if secenek == "1":
+        print("Dörtgen Seçtiniz.\n        Dörtgen Hesaplama\n**********************************\n")
+        kenar1 = int(input("1.Kenarı giriniz"))
+        kenar2 = int(input("2.Kenarı giriniz"))
+        kenar3 = int(input("3.Kenarı giriniz"))
+        kenar4 = int(input("4.Kenarı giriniz"))
+        if kenar1 == kenar2 == kenar3 == kenar4:
+            print("Dörtgeniniz bir kare.")
+        elif kenar1 == kenar2 and kenar3 == kenar4:
+            print("Dörtgeniniz bir dikdörtgen.")
+        elif kenar2 == kenar3 and kenar1 == kenar4:
+            print("Dörtgeniniz bir dikdörtgen.")
+        else:
+            print("Dörtgeniniz sıradan bir dörtgen.")
+        break;
+    elif secenek == "2":
+        print("Üçgen seçtiniz.\n          Üçgen Hesaplama\n**********************************")
+        kenar1 = int(input("1.Kenarı giriniz"))
+        kenar2 = int(input("2.Kenarı giriniz"))
+        kenar3 = int(input("3.Kenarı giriniz"))
+        if abs(kenar1+kenar2) > kenar3 and abs(kenar1+kenar3) > kenar2 and abs(kenar2+kenar3) > kenar1:
+            if kenar1 == kenar2 and kenar1 == kenar3:
+                print("Üçgenininz bir eşkenar üçgen.")
+            elif ((kenar1 == kenar2 and kenar1 != kenar3) or (kenar1 == kenar3 and kenar1 != kenar2) or (kenar3 == kenar2 and kenar3 != kenar1)):
+                print("Üçgeniniz bir ikizkenar üçgen.")
+            else:
+                print("Üçgeniniz bir çeşitkenar üçgen.")
+        else:
+            print("Üçgen değil.")
+        break;
+    else:
+        print("Böyle bir seçenek bulunmamakta.\nTekrar giriniz.")
+print("Program sonlandırıldı.")
+#Problem çözüldü.
