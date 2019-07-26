5117341c3d4ca493a85a719642b94aded52d9c39
M. E. Berkay KOCAOĞLU <50452706+mebon@users.noreply.github.com>
2019-07-26 20:33:49 +0300

    Problem3_3 çözüldü.

--- Problemler3/Problem3_3.py
+++ Problemler3/Problem3_3.py
@@ -3,3 +3,13 @@
 
 İpucu: İç içe 2 tane for döngüsü kullanın. Aynı zamanda sayıları range() fonksiyonunu kullanarak elde edin.
 """
+print("****************************\n"
+      "      Çarpım Tablosu\n"
+      "****************************")
+sayilar=range(1,11)
+
+for x in sayilar:
+    for y in sayilar:
+        print(x,"x",y,"=",x*y)
+    print("****************************")
+#Problem çözüldü.
