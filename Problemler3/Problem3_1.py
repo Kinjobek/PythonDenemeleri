68d2d2623e33024cdd1c7a1640f017185be38034
M. E. Berkay KOCAOĞLU <50452706+mebon@users.noreply.github.com>
2019-07-26 20:08:41 +0300

    Problem3_1 çözüldü.

--- Problemler3/Problem3_1.py
+++ Problemler3/Problem3_1.py
@@ -3,3 +3,20 @@ Kullanıcıdan aldığınız bir sayının mükemmel olup olmadığını bulmaya
 
 Bir sayının kendi hariç bölenlerinin toplamı kendine eşitse bu sayıya "mükemmel sayı" denir. Örnek olarak, 6 mükemmel bir sayıdır. (1 + 2 + 3 = 6)
 """
+print("*****************************\n"
+      "       Mükemmel Sayı\n"
+      "*****************************")
+sayi = int(input("Sayı giriniz : "))
+sayiListesi = range(1,sayi)
+tamBölenler = list()
+toplam = 0
+for x in sayiListesi:
+    if sayi % x == 0:
+        tamBölenler.append(x)
+print(tamBölenler)
+for i in tamBölenler:
+    toplam += i
+if sayi == toplam :
+    print(sayi,"bir mükemmel sayıdır.")
+else :
+    print(sayi ,"bir mükemmel sayı değildir.\nToplamları : {}".format(toplam))
