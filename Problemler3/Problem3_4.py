ec3da8d6394e24eea518a0e761784e560cf6228e
M. E. Berkay KOCAOĞLU <50452706+mebon@users.noreply.github.com>
2019-07-26 20:45:54 +0300

    Problem3_4 çözüldü.

--- Problemler3/Problem3_4.py
+++ Problemler3/Problem3_4.py
@@ -5,3 +5,26 @@ Kullanıcı "q" tuşuna bastığı zaman döngüyü sonlandırın ve ekrana "top
 
 İpucu : while döngüsünü sonsuz koşulla başlatın ve kullanıcı q'ya basarsa döngüyü break ile sonlandırın.
 """
+print("**********************************\n"
+      "     Dur Diyene Kadar Topla\n"
+      "**********************************\n"
+      "               Menü\n"
+      "Çıkış için 'q' yazınız.\n"
+      "**********************************")
+girilenSayilar = list()
+toplam = 0
+while True:
+    girilenDeger = input("Sayı giriniz :")
+
+    if(girilenDeger == "q"):
+        print("Toplam : {}".format(toplam))
+        print("Sayılar : ",*girilenSayilar)
+        break
+    elif girilenDeger.isnumeric():
+        girilenSayilar.append(int(girilenDeger))
+        toplam += int(girilenDeger)
+    else:
+        print("Hatalı değer girdiniz.")
+
+#Problem çözüldü.
+
