b32dbbba3dd4726db99f40d5e28e9dae3d4f935b
M. E. Berkay KOCAOĞLU <50452706+mebon@users.noreply.github.com>
2019-07-27 23:16:47 +0300

    Problem4_2.py için alan oluşturuldu.

---
+++ Problemler4/Problem4_2.py
@@ -0,0 +1,6 @@
+"""Kullanıcıdan 2 tane sayı alarak bu sayıların en büyük ortak bölenini (EBOB) dönen bir tane fonksiyon yazın.
+
+Problem için şu siteye bakabilirsiniz;
+
+http://www.matematikciler.com/6-sinif/matematik-konu-anlatimlari/1020-en-kucuk-ortak-kat-ve-en-buyuk-ortak-bolen-ebob-ekok
+""""
