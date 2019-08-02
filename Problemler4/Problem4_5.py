e90cde3136f0e12cd65b85c980638bf019d10ec0
M. E. Berkay KOCAOĞLU <50452706+mebon@users.noreply.github.com>
2019-08-02 20:18:34 +0300

    Problem4_5.py çözüldü.

--- Problemler4/Problem4_5.py
+++ Problemler4/Problem4_5.py
@@ -1,3 +1,17 @@
 """
 1'den 100'e kadar olan sayılardan pisagor üçgeni oluşturanları ekrana yazdıran bir fonksiyon yazın.(a <= 100,b <= 100)
 """
+def pisagor():
+
+    cList = list()
+    for i in range(1,101):
+        for j in range(1,101):
+            c = (i ** 2 + j ** 2) ** 0.5
+            if c == int(c):
+                cList.append((i,j,int(c)))
+
+    return cList
+
+for i in pisagor():
+    print(i)
+#Problem Çözüldü
