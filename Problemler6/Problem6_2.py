37afeebae998a58b34d5ad1292bdb6db811625aa
M. E. Berkay KOCAOĞLU <50452706+mebon@users.noreply.github.com>
2019-08-12 20:41:48 +0300

    Problem6_2.py ekleme yapıldı.

--- Problemler6/Problem6_2.py
+++ Problemler6/Problem6_2.py
@@ -16,13 +16,17 @@ class Bilgisayar():
         self.durum = "Kapalı"
     def menuYazdir(self):
         if self.bilgisayarDurum() == "Kapalı":
-            print("\n1) Bilgisayarı aç\n"
+            print("1) Bilgisayarı aç\n"
                   "q) Programı kapat")
         elif self.bilgisayarDurum() == "Açık":
             print("1) Masaüstü göster\n"
                   "2) Bilgisayarım göster\n"
                   "3) Belgeler göster\n"
-                  "4) Bilgisayarı kapat")
+                  "4) Konumu göster\n"
+                  "5) \n"
+                  "6) \n"
+                  "7) \n"
+                  "q) ")
     def bilgisayarAcKapat(self):
         if self.durum == "Kapalı":
             i=0
@@ -40,20 +44,32 @@ class Bilgisayar():
                 i += 1
                 time.sleep(0.30)
             self.durum = "Kapalı"
+            print("")
+    print("******************************")
     def bilgisayarDurum(self):
         return self.durum
     def masaüstüDosyalarıGoster(self):
         for i in self.dosyalar["Masaüstü"]:
-            print(self.dosyalar["Masaüstü"][str(i)])
+            print(i,")",self.dosyalar["Masaüstü"][str(i)])
             time.sleep(0.5)
+        print("******************************")
     def bilgisayarımDosyalarıGoster(self):
         for i in self.dosyalar["Bilgisayarım"]:
-            print(self.dosyalar["Bilgisayarım"][str(i)])
+            print(i,")",self.dosyalar["Bilgisayarım"][str(i)])
             time.sleep(0.5)
+        print("******************************")
     def belgelerDosyalarıGoster(self):
         for i in self.dosyalar["Belgeler"]:
-            print(self.dosyalar["Belgeler"][str(i)])
+            print(i,")",self.dosyalar["Belgeler"][str(i)])
             time.sleep(0.5)
+        print("******************************")
+    def konumuGoster(self):
+        time.sleep(0.5)
+        print("Konum : ",self.konum)
+        time.sleep(0.5)
+        print("Konumda bulunanlar : ",*self.dosyalar[self.konum].items())
+        time.sleep(0.5)
+        print("******************************")
     def __str__(self):
         return self.konum
 
@@ -74,6 +90,8 @@ def baslatPC():
                 elif secenek == "3":
                     pc.belgelerDosyalarıGoster()
                 elif secenek == "4":
+                    pc.konumuGoster()
+                elif secenek == "q":
                     pc.bilgisayarAcKapat()
                     break
         elif secenek != "1" and secenek != "q":
