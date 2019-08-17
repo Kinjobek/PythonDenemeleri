b96eec76adb8d0caa9634b88044b5b82af6e6e5f
M. E. Berkay KOCAOĞLU <50452706+mebon@users.noreply.github.com>
2019-08-17 19:15:14 +0300

    Problem9_4.py çözüldü.

--- Problemler9/Problem9_4.py
+++ Problemler9/Problem9_4.py
@@ -9,3 +9,11 @@ Bu isimleri ve soyisimleri sırasıyla eşleştirin ve ekrana alt alta isimleri 
 
 Not: zip() fonksiyonunu kullanmaya çalışın.
 """
+isimler = ["Kerim","Tarık","Ezgi","Kemal","İlkay","Şükran","Merve"]
+soyisimler = ["Yılmaz","Öztürk","Dağdeviren","Atatürk","Dikmen","Kaya","Polat"]
+
+isimlerVeSoyisimler = list(zip(isimler,soyisimler))
+
+for i,j in isimlerVeSoyisimler:
+    print("İsim : {}/{} : Soyisim\n".format(i,j))
+#Problem çözüldü.
