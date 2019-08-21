e7e976743164cb3843e4a977ee0a93db099de056
M. E. Berkay KOCAOĞLU <50452706+mebon@users.noreply.github.com>
2019-08-22 00:09:31 +0300

    Islem güncellendi.

--- SMTPMailIslemi/İslem.py
+++ SMTPMailIslemi/İslem.py
@@ -6,45 +6,35 @@ import sys
 """
 SMTP Modülü ile mail gönderme
 
-İlk olarak daha az güvenli uygulamalar için öncelikle aşağıdaki linke gidiyoruz ve güvenliği
-kaldırıyoruz.
-
-https://myaccount.google.com/lesssecureapps
+Gmail'den daha az güvenli uygulamalar için güvenliği kaldır. -> https://myaccount.google.com/lesssecureapps
 
 """
 
-mesaj = MIMEMultipart()  # Mail yapımızı oluşturuyoruz.
+mesaj = MIMEMultipart()  # Mail yapısı
 
-mesaj["From"] = "bekomeb95@gmail.com"  # Kimden Göndereceğimiz
+mesaj["From"] = "...@gmail.com"  # Kimden
+mesaj["To"] = "....@windowslive.com"  # Kime
+mesaj["Subject"] = "Smtp Message"  # Mail Konusu
 
-mesaj["To"] = "bekomeb95@gmail.com"  # Kime Göndereceğimiz
+icerik = """
 
-mesaj["Subject"] = "Smtp Mail Gönderme"  # Mailimizin Konusu
-
-# Mailimizin İçeriği
-yazi = """
-
-Merhaba, Python ile mail gönderiyorum.    
-
-Mustafa Murat Coşkun
+Smtp ile mesaj deneme.
+gmail ve hotmail gönderimi basarili.
+Sira windowslive'da(basarili)
+Mebon
 
 
 """
-
-mesaj_govdesi = MIMEText(yazi, "plain")  # Mailimizin gövdesini bu sınıftan oluşturuyoruz.
-
-mesaj.attach(mesaj_govdesi)  # Mailimizin gövdesini mail yapımıza ekliyoruz.
+mesaj_icerigi = MIMEText(icerik, "plain")  # Mailimizin gövdesini bu sınıftan oluşturuyoruz.
+mesaj.attach(mesaj_icerigi)  # Mailimizin gövdesini mail yapımıza ekliyoruz.
 
 try:
     mail = smtplib.SMTP("smtp.gmail.com", 587)  # SMTP objemizi oluşturuyoruz ve gmail smtp server'ına bağlanıyoruz.
-
     mail.ehlo()  # SMTP serverına kendimizi tanıtıyoruz.
-
     mail.starttls()  # Adresimizin ve Parolamızın şifrelenmesi için gerekli
 
-    mail.login("gmail_adresi",
-               "gmail_parola")  # SMTP server'ına giriş yapıyoruz. Kendi mail adresimizi ve parolamızı yapıyoruz.
-
+    mail.login("....@gmail.com",
+               "sifre")  # SMTP server'ına giriş yapıyoruz. Kendi mail adresimizi ve parolamızı yapıyoruz.
     mail.sendmail(mesaj["From"], mesaj["To"], mesaj.as_string())  # Mailimizi gönderiyoruz.
     print("Mail başarıyla gönderildi....")
     mail.close()  # Smtp serverımızın bağlantısını koparıyoz.
