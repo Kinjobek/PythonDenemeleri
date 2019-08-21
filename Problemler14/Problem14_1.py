293c85871d765c0e292f0abc8f4aa0ca7d6b1767
M. E. Berkay KOCAOĞLU <50452706+mebon@users.noreply.github.com>
2019-08-22 00:38:42 +0300

    Problemler14 için alan oluşturuldu.

---
+++ Problemler14/Problem14_1.py
@@ -0,0 +1,2 @@
+"""Bilgisayarınızdaki tüm mp4,txt ve pdf dosyalarını os modülüyle arayın ve bunların nerede bulunduklarını ve isimlerini ayrı ayrı
+"pdf_dosyalari.txt","mp4_dosyaları.txt","txt_dosyaları.txt" adlı dosyalara kaydedin."""
